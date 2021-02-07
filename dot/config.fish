d9985b97f534232d573398d9e272d1eea1d11397
JingYu Ning <foldfelis@gmail.com>
2021-02-08 03:01:44 +0800

    conda env

--- dot/config.fish
+++ dot/config.fish
@@ -66,3 +66,8 @@ alias yt-f="youtube-dl -f "
 
 ### SETTING THE STARSHIP PROMPT ###
 starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval ~/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
